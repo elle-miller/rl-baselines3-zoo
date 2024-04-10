d5300d07d2d7ed84397930f65c1a5390aa0a56bb
elle-miller <elle.miller101@gmail.com>
2024-04-10 17:33:11 +0000

    merge

--- rl_zoo3/import_envs.py
+++ rl_zoo3/import_envs.py
@@ -63,18 +63,31 @@ for env_id in MaskVelocityWrapper.velocity_indices.keys():
         entry_point=create_no_vel_env(env_id),  # type: ignore[arg-type]
     )
 
-# added
-from internal.tactile_testing.src.utils import HandManipulateEggWrapper
 
-def create_inhand_env(env_id: str) -> Callable[[Optional[str]], gym.Env]:
-    def make_env(render_mode: Optional[str] = None) -> gym.Env:
-        env = gym.make(env_id, render_mode=render_mode)
-        env = HandManipulateEggWrapper(env)
-        return env
-    return make_env
+# def create_inhand_env(env_id: str) -> Callable[[Optional[str]], gym.Env]:
+#     def make_env(render_mode: Optional[str] = None) -> gym.Env:
+#         env = gym.make(env_id, render_mode=render_mode)
+#         env = HandManipulateEggWrapper(env)
+#         return env
+#     return make_env
 
-env_id = "HandManipulateEgg_TouchGrid-v1"
-register(
-    id=env_id,
-    entry_point=create_inhand_env(env_id),  # type: ignore[arg-type]
-)
+# env_id = "HandManipulateEgg_TouchGrid-v1"
+# register(
+#     id=env_id,
+#     entry_point=create_inhand_env(env_id),  # type: ignore[arg-type]
+# )
+
+# import touch_gym
+
+
+
+# register(
+#     id=f"HandManipulateEgg_TouchGrid-v1",
+#     entry_point="touch_gym.envs.hme_touchgrid:MujocoHandEggTouchGridSensorsEnv",
+#     kwargs=
+#         {
+#             "target_position": "random",
+#             "target_rotation": "xyz",
+#         },
+#     max_episode_steps=100,
+# )
